105898e4d5d00c77b1f0f70a4d5a9d652aeaacf8
ramprasadk69-dev <ramprasadk69@gmail.com>
2026-02-26 20:59:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,7 @@
-const cards = document.querySelectorAll(".card");
+function goToOrderForm() {
+  window.location.href = "order.html";
+}
 
-cards.forEach(card => {
-  card.addEventListener("click", () => {
-    const title = card.querySelector("h2").innerText;
-    alert(title + " clicked");
-  });
-});
 
 
 
